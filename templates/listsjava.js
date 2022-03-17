d4eaf81b834dd33977d7890ca56ee366b7bda7a5
ConnorR6 <91033641+ConnorR6@users.noreply.github.com>
2022-03-16 19:44:45 -0700

    Create listsjava.js

---
+++ templates/listsjava.js
@@ -0,0 +1,18 @@
+function loadlist(){
+var table = '';
+       table += "<tr>";
+        	table += "<td>Outback Steakhouse	</td>";
+            table += "<td>177 W San Marcos Blvd, San Marcos, CA	</td>";
+            table += "<td>11AM-10pm	</td>";
+            table += "<td>(760) 916-1126	</td>";
+        table += "</tr>";
+        
+        table += "<tr>";
+        	table += "<td>KFC	</td>";
+            table += "<td>771 Center Dr, San Marcos, CA	</td>";
+            table += "<td>10am-11pm	</td>";
+            table += "<td>760) 781-3384	</td>";
+        table += "</tr>";
+        
+        document.write("<table>" + table + "</table>");
+}
